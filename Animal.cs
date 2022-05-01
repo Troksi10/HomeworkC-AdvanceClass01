ee045f64affd614c6c6a9eabdcbb2f4096eb3a0e
Troksi10 <98901818+Troksi10@users.noreply.github.com>
2022-05-02 00:20:25 +0200

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,28 @@
+﻿using HomeworkClass01.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkClass01
+{
+    abstract class Animal : IAnimal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Type { get; set; }
+
+        public abstract void PrintAnimal();
+
+        public List<string> AnimalTypes { get; set; } = new List<string>();
+
+        public Animal (string name,int age,string type)
+        {
+            Name = name;
+            Age = age;
+            Type = type;
+
+        }
+    }
+}
